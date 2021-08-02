ee8b4d500a7554c20f8ad82417b96b1a641cecf5
Connor Lee <45024235+connorleee@users.noreply.github.com>
2021-08-01 21:16:06 -0700

    broke init method into smaller functions. deleted unnecessary code to clean up.

--- public/index.js
+++ public/index.js
@@ -8,49 +8,54 @@ recordButton.addEventListener("mousedown", (e) => audioRecorder.start(e));
 recordButton.addEventListener("mouseup", (e) => audioRecorder.stop(e));
 
 const audioRecorder = {
-  // stream: null,
   recorder: null,
   recordedChunks: [],
-  init: function () {
-    audioRecorder.recorder = null;
+  init: async function () {
     audioRecorder.recordedChunks = [];
-  },
-  start: async function (e) {
+
     try {
-      const device = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: false,
-      });
+      const device = await navigator.mediaDevices.getUserMedia({ audio: true });
 
       audioRecorder.recorder = new MediaRecorder(device);
-      audioRecorder.recorder.onstop = async function (e) {
-        const blob = new Blob(audioRecorder.recordedChunks, {
-          type: "audio/ogg; codecs=opus",
-        });
-        const audioURL = window.URL.createObjectURL(blob);
-
-        let recording = await postAudio(audioURL).then((res) => res.json());
-        createAudioEl(recording);
-      };
+      audioRecorder.recorder.onstop = handleStop;
       audioRecorder.recorder.addEventListener("dataavailable", (e) => {
         if (e.data.size > 0) {
           audioRecorder.recordedChunks.push(e.data);
         }
       });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  start: async function (e) {
+    try {
+      await audioRecorder.init();
 
       audioRecorder.recorder.start();
     } catch (err) {
       console.log(err);
     }
   },
-  stop: async function (e) {
-    e.preventDefault();
-
+  stop: function () {
     audioRecorder.recorder.stop();
-
-    audioRecorder.init();
   },
 };
+
+audioRecorder.init();
+
+async function handleStop() {
+  const blob = new Blob(audioRecorder.recordedChunks, {
+    type: "audio/ogg; codecs=opus",
+  });
+  const audioURL = window.URL.createObjectURL(blob);
+
+  try {
+    let recording = await postAudio(audioURL).then((res) => res.json());
+    createAudioEl(recording);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 function createAudioEl(audioURL) {
   let recordingsList = document.getElementById("recordings-list");
@@ -70,19 +75,4 @@ function postAudio(recordingURL) {
     },
     body: JSON.stringify({ recordingURL }),
   }).catch((err) => console.log("post error", err));
-}
-
-function getAudioURLs() {
-  return (
-    fetch("http://localhost:3000/audio")
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          console.log("Failed to get audio URLs");
-        }
-      })
-      // .then((data) => console.log(data))
-      .catch((err) => console.log("get error", err))
-  );
 }
